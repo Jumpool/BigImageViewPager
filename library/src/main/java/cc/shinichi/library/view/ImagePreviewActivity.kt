2eb231a90b5e8b70b65f66d7f810165e2e54921a
Jumpool <36327172+Jumpool@users.noreply.github.com>
2023-09-05 21:54:24 +0200

    Update ImagePreviewActivity.kt
    Fullscreen and hide nav & status bar

--- library/src/main/java/cc/shinichi/library/view/ImagePreviewActivity.kt
+++ library/src/main/java/cc/shinichi/library/view/ImagePreviewActivity.kt
@@ -524,10 +524,15 @@ class ImagePreviewActivity : AppCompatActivity(), Handler.Callback, View.OnClick
             }
         }
         val decorView = window.decorView
-        val vis = decorView.systemUiVisibility
-        val option =
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        decorView.systemUiVisibility = vis or option
+        //val vis = decorView.systemUiVisibility
+        val option = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            //View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        decorView.systemUiVisibility = option //vis or option
     }
 
     companion object {
